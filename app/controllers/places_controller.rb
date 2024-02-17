54936e4f28711d4146f798a9b07f29c5c914f2a2
Ankush  Halbe <ankush.halbe@kellogg.northwestern.edu>
2024-02-17 02:57:32 +0000

    cc

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,21 @@
 class PlacesController < ApplicationController
   def index 
     render :template => "places/index"
+    @places = Places.all
   end
+end
+
+def show
+   @place = Places.find_by({"id" => params["id"]})
+end
+
+def new
+
+end
+
+def create
+@places = Places.new
+@places["name"] = params ["name"]
+@places.save
+redirect_to "/places"
 end
